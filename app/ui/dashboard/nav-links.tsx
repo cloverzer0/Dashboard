afa0b5bdf75b03749c3a3cbcc2f22e3e628d7693
Prunellie Tchakam <prunellie578@gmail.com>
2025-01-07 20:22:29 -0500

    changed app name and invoice to my tasks

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -14,8 +14,8 @@ import clsx from 'clsx';
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
+    name: 'My Tasks',
+    href: '/dashboard/mytasks',
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
